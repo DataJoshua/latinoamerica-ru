2c1b6d0eeacf16a10aeb3f6e78829c3cf7f95e18
Julian <juli2003juli@gmail.com>
2024-03-23 21:13:32 +0300

    .idea

--- src/atoms/Button.jsx
+++ src/atoms/Button.jsx
@@ -1,6 +1,11 @@
 import "../styles/button.css"
-const Button = () => {
-  return <button className="button">Click</button>
+const Button = ({label}) => {
+  return <button
+      className={button}
+      onClick={handleOnClick}
+  >
+    {label}
+  </button>
 }
 
 export default Button;
