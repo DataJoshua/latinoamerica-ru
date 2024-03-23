95485d184e9e16a1efc71e1f0d88b831f8c7b172
DataJoshua <rodriguez.taco.joshua@gmail.com>
2024-03-23 18:54:02 +0400

    correcion de estilos del slider

--- src/components/splide/SliderWrapper.jsx
+++ src/components/splide/SliderWrapper.jsx
@@ -4,9 +4,9 @@ import Slides from './Slides';
 const SliderWrapper = () => {
   return (
     <>
-      <div className="w-full h-full flex justify-center bg-slate-100 sm:px-2 py-4">
+      <div className="w-full h-full flex justify-center bg-slate-100 sm:px-2 sm:py-4">
         <Splide
-          className="w-full md:w-[1000px] h-[500px] rounded-lg overflow-hidden" 
+          className="w-full md:w-[1000px] h-[35vh] sm:h-[50vh] rounded-lg overflow-hidden" 
           options={{
             rewind: true,
             drag: true,
