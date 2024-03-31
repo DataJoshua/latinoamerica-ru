0448fed28185bdc120e9127c789ee00ecd8ccb7f
Julian <juli2003juli@gmail.com>
2024-03-31 19:05:07 +0300

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import CountriesPage from "./pages/CountriesPage";
 import EventsPage from "./pages/EventsPage";
 import MainPage from "./pages/MainPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "./atoms/footer/Footer";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
           <Route path="/eventos" element={<EventsPage/>}></Route>
           <Route path="/paises" element={<CountriesPage/>}></Route>
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </>
   );
