bdcd7e8005d9055580afd65f4bc3f1d2a81f57d1
RevxngE4 <andrespisa4@gmail.com>
2024-05-10 16:37:09 +0300

    espero que ya sea el final de esta monda

--- src/components/Carousel/Title.jsx
+++ src/components/Carousel/Title.jsx
@@ -16,25 +16,37 @@ const Title = ({ handleOnFormActive, isActive }) => {
   const content = [
     {
       author: "Латиноc",
-      title: "День Латинской Америки",
+      title: "Дни латиноамериканской культуры в КФУ",
       topic: "",
-      description: "25 Апреля\n\n" +
-          "13:00 - 17:00  Спортивный день \n               Турнир по футболу\n\n" +
-          "26 Апреля\n\n" +
-          "16:00 - 17:00 Выставка \"Латинская Америка\"  \n" +
-          "17:00 - 19:00 Концерт \"День Латинской Америки\"\n",
+      description: "18 мая 2024 г. в 11:30 спортивное мероприятие «мини – футбол» \n\n" +
+          "Место проведения: КСК КФУ УНИКС (спортивный зал) Адрес: г. Казань, улица Профессора Нужина, 2\n" +
+          "Регистрация для гостей и зрителей до 12 мая 2024 г до 20:00 \n\n" +
+          "24 мая 2024 г. Гала - концерт «Путешествие по Латинской Америке»\n\n" +
+          "в 15:30 Интерактивная выставка \"Знакомство с Латинской Америкой\"\n" +
+          "в 16:30 – праздничный концерт \n" +
+          "Место проведения: Института филологии и межкультурной коммуникации КФУ (актовый зал) Адрес: г. Казань, улица Татарстан, 2.\n" +
+          "Регистрация для гостей и зрителей до 19 мая 2024 г до 20:00 \n\n" +
+          "29 мая 2024 г.  в 16:00 - круглый стол: «Основные проблемы, с которыми сталкиваются студенты из Латинской Америки в процессе адаптации в России» в рамках X Международного форума по педагогическому образованию в КФУ (гибридный формат - с последовательным переводом)\n" +
+          "Место проведения: Институт психологии и образования КФУ, аудитория 105 Адрес: г. Казань, улица Межлаука 1.  \n" ,
       register: "Хочу присутствовать!"
     },
     {
       author: "LATINOS",
-      title: "DIA DE LATINOAMERICA",
+      title: "Días de la Cultura Latinoamericana en KFU ",
       topic: "",
-      description: "25 DE ABRIL\n\n" +
-          "13:00 - 17:00  Jornada deportiva\n" +
-          "Torneo relámpago de futbol\n\n" +
-          "26 DE ABRIL\n\n" +
-          "16:00 - 17:00 Pabellón \"America Latina\" \n" +
-          "17:00 - 19:00 Concierto \"Día de Latinoamérica\"\n",
+      description: "18 de mayo de 2024 a las 11:30 evento deportivo “mini fútbol\n\n" +
+          "Lugar: KSK KFU UNICS (sala para deportes) Dirección: Kazán, calle Profesor Nuzhin, 2 \n" +
+          "Inscripciones para invitados y espectadores hasta el 12 de mayo de 2024 a las 20:00 horas\n\n" +
+          "24 de mayo de 2024 Gala - concierto “Viaje por América Latina”\n\n" +
+          "15:30 Exposicion interactiva \"Un viaje por América Latina\" \n" +
+          "16:30 Concierto de celebracion\n" +
+          "Lugar: Instituto de Filología y Comunicación Intercultural de la KFU (salón de actos) Dirección: Kazán, calle Tatarstán, 2.\n" +
+          "Inscripciones para invitados y espectadores hasta el 19 de mayo de 2024 a las 20:00 horas \n\n" +
+          "29 de mayo de 2024 a las 16:00 - mesa redonda: “Los principales problemas que enfrentan los estudiantes de América Latina en el proceso de adaptación a Rusia” " +
+          "en el marco del X Foro Internacional de Formación Docente en la KFU (formato híbrido - con traducción consecutiva )\n\n" +
+          "Lugar: Instituto de Psicología y Educación KFU, auditorio 105 Dirección: Kazán, calle Mezhlauka 1.\n",
+
+
       register: "Quiero Asistir!"
     }
   ]
