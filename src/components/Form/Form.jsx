0197c71bb73d69c79edffad996cc0fac3e11aa65
RevxngE4 <andrespisa4@gmail.com>
2024-05-21 12:10:10 +0300

    form a mimir

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,10 +1,9 @@
 import React, { useState } from 'react';
-import Button from "../../atoms/Button.jsx";
+/*import Button from "../../atoms/Button.jsx";*/
 import "../../styles/Form.css";
 import "../../styles/Success.css"
-import Spinner from '../../atoms/Spinner.jsx';
-import universidades from './universidades.json';
-import Timer from '../Timer.jsx';
+/*import Spinner from '../../atoms/Spinner.jsx';
+import universidades from './universidades.json';*/
 
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
@@ -27,11 +26,11 @@ function Form({ isActive, handleOnFormDismiss = ()=> {} }) {
     const handleOcupacionChange = (e) => {
         setOcupacion(e.target.value)
     }
-    
+
     const getUniversityValue = () => {
         if(otherUniversity) return customUni
         if(noUniversity) return "Пустой"
-        
+
         return university
     }
 
@@ -140,11 +139,10 @@ function Form({ isActive, handleOnFormDismiss = ()=> {} }) {
         setFormSubmitted(false);
     };
 
-    return (
+    /*return (
         <div className={`${isActive ? "" : "hidden"} lg:block`}>
             {!formSubmitted ? (
                 <div className="form-container scale-[0.90] sm:scale-100 top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] sm:translate-x-[0px] sm:translate-y-[0px] sm:left-[10%] sm:top-[17%] lg:left-[15%]">
-                    <Timer targetHour={12} targetMinute={0} targetSecond={0} />
                     <p className="animate-pulse py-2 text-red-800 text-md">Por favor llenar el nombre y apellido solo en <span className="font-bold">Ruso</span></p>
                     <form onSubmit={handleSubmit} className="form">
                         <div className="form-group">
@@ -231,7 +229,7 @@ function Form({ isActive, handleOnFormDismiss = ()=> {} }) {
                 </div>
             )}
         </div>
-    );
+    );*/
 }
 
 export default Form;
